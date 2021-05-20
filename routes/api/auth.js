85aa0c3daef596b94843e7c07038c7c34cf49c78
Justin Stoddard <justin.stoddard@go1.com>
2021-05-19 19:11:48 -0600

    added auth middleware with jwt

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
-// @route GET api/auth
-// @desc Test Route
-// @access public
-router.get('/', (req, res) => {
+// @route     GET api/auth
+// @desc      Test Route
+// @access    public
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  }
+  catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
   res.send('Auth Route');
 });
 
