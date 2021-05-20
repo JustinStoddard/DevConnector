04b5981cfe8db95b87be9d68c3169a4a96ed22d6
Justin Stoddard <justin.stoddard@go1.com>
2021-05-19 18:18:41 -0600

    added user model

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-//@route GET api/users
-//@desc Test Route
-//@access public
+// @route GET api/posts
+// @desc Test Route
+// @access public
 router.get('/', (req, res) => {
   res.send('Posts Route');
 });
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,9 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 
-//@route GET api/users
-//@desc Test Route
-//@access public
+// @route GET api/profile
+// @desc Test Route
+// @access public
 router.get('/', (req, res) => {
   res.send('Profile Route');
 });
