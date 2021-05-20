3b078138e411543bc151d7fa5fe5805600b44828
Justin Stoddard <justin.stoddard@go1.com>
2021-05-19 18:31:25 -0600

    initialized middleware for express server - created validation for register user endpoint

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect Database
 connectDB();
 
+//Init Middelware
+app.use(express.json({ extended: false }))
+
 app.get('/', (req, res) => {
   res.send('API Running');
 });
