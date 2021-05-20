db5d4b151cf617909a6e475fa4b0a564b1291819
Justin Stoddard <justin.stoddard@go1.com>
2021-05-19 18:13:01 -0600

    added routes

--- config/db.js
+++ config/db.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const config = require('config');
 const db = config.get('mongoURI');
 
-
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
---
+++ routes/api/auth.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+//@route GET api/users
+//@desc Test Route
+//@access public
+router.get('/', (req, res) => {
+  res.send('Auth Route');
+});
+
+module.exports = router;
